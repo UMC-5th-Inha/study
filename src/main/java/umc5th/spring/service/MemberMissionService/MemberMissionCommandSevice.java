c1bfa14b79799470cc6ed812f15013343aff9e2f
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-11-18 22:39:51 +0900

    feat : create MemberMissionCommandSeviceC

---
+++ src/main/java/umc5th/spring/service/MemberMissionService/MemberMissionCommandSevice.java
@@ -0,0 +1,11 @@
+package umc5th.spring.service.MemberMissionService;
+
+import java.util.List;
+import umc5th.spring.domain.Mission;
+import umc5th.spring.domain.mapping.MemberMission;
+
+public interface MemberMissionCommandSevice {
+    List<Mission> getAvailableMissions(Long memberId);
+
+    MemberMission addMemberMission(Long memberId, Long missionId);
+}
