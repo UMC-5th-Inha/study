dc5911667c9734743fd2288f2f45ff758772a0f7
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-11-18 22:38:45 +0900

    feat : create MemberMissionRestController

---
+++ src/main/java/umc5th/spring/web/controller/MemberMissionRestController.java
@@ -0,0 +1,32 @@
+package umc5th.spring.web.controller;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import umc5th.spring.apiPayload.ApiResponse;
+import umc5th.spring.converter.MemberMissionConverter;
+import umc5th.spring.domain.Mission;
+import umc5th.spring.domain.mapping.MemberMission;
+import umc5th.spring.service.MemberMissionService.MemberMissionCommandSevice;
+import umc5th.spring.web.dto.MemberMissionResponseDTO;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class MemberMissionRestController {
+
+    MemberMissionCommandSevice memberMissionCommandSevice;
+
+    @GetMapping("/{memberId}/missions/")
+    public List<Mission> getAvailableMissions(@PathVariable Long memberId) {
+        return memberMissionCommandSevice.getAvailableMissions(memberId);
+    }
+
+    @PostMapping("/{memberId}/missions/{missionId}")
+    public ApiResponse<MemberMissionResponseDTO.addMemberMissionResultDTO> addMemberMission(
+            @PathVariable Long memberId, @PathVariable Long missionId) {
+        MemberMission savedMemberMission = memberMissionCommandSevice.addMemberMission(memberId, missionId);
+
+        return ApiResponse.onSuccess(MemberMissionConverter.toAddMemberMissionResultDTO(savedMemberMission));
+    }
+}
